7ba12570d21a95ed8d4cce4ef436c9ae8634ea2d
BCMPF <BCMPF@gmail.com>
2023-04-19 10:04:05 -0400

    site responsive + crud ok

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ import routes from './routes/route.js';
 const app = express();
 
 app.use(cors());
-
-
 app.use(bodyParser.json({ extended: true } ));
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use('/', routes);
 
 const PORT = 8000;
-
 Connection();
-
 app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
